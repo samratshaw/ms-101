81eadb7d641477076025b1136d217eaea6cac64e
samrat.shaw <samrat.shaw@accenture.com>
2020-12-30 02:03:35 +0700

    [#2] Added spring cloud config server

--- helpers/msInitializer.sh
+++ helpers/msInitializer.sh
@@ -1,12 +1,25 @@
-# Spring initializer with Spring Cloud Config
+# Spring initializer
+#spring init \
+#--boot-version=2.3.7 \
+#--build=maven \
+#--java-version=11 \
+#--packaging=jar \
+#--name=product-service \
+#--package-name=com.samrat.ms.core \
+#--groupId=com.samrat.ms.core \
+#--dependencies=web,actuator,lombok \
+#--version=1.0.0 \
+#product-service
+
+# Config Server
 spring init \
 --boot-version=2.3.7 \
 --build=maven \
 --java-version=11 \
 --packaging=jar \
---name=product-service \
+--name=spring-config-server \
 --package-name=com.samrat.ms.core \
 --groupId=com.samrat.ms.core \
---dependencies=web,actuator,lombok \
+--dependencies=web,actuator,cloud-config-server \
 --version=1.0.0 \
-product-service
+spring-config-server
