c9a5ff107468af0a26cc2d477fd1dbc6faf82ea9
samrat.shaw <samrat.shaw@accenture.com>
2020-12-29 21:12:53 +0700

    [#2] Added base project for Product Service

---
+++ helpers/msInitializer.sh
@@ -0,0 +1,12 @@
+# Spring initializer with Spring Cloud Config
+spring init \
+--boot-version=2.3.7 \
+--build=maven \
+--java-version=11 \
+--packaging=jar \
+--name=product-service \
+--package-name=com.samrat.ms.core \
+--groupId=com.samrat.ms.core \
+--dependencies=web,actuator,lombok \
+--version=1.0.0 \
+product-service
